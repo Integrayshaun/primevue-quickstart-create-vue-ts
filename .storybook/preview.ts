904fd395cb1780951961a16570041fbba7cb6778
Shaun Lloyd <shaun@chromatic.com>
2023-02-17 15:48:06 -0500

    Add PrimeVue and styles to storybook
    Everything that you imported into src/main.ts should be imported and registered with Storybook's vue app

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,3 +1,23 @@
+import { setup } from "@storybook/vue3";
+
+import PrimeVue from "primevue/config";
+import Button from "primevue/button";
+import Toast from "primevue/toast";
+import ToastService from "primevue/toastservice";
+
+import "../src/assets/main.css";
+import "primevue/resources/themes/saga-blue/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+
+setup((app) => {
+  app.use(PrimeVue);
+  app.use(ToastService);
+
+  app.component("Button", Button);
+  app.component("Toast", Toast);
+});
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
